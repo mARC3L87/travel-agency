dbcdb9484500a77a958f90bfe695983b3d4a9f07
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-07-15 17:02:44 +0200

    Add send order to API

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,12 +5,55 @@ import { Row, Col } from 'react-flexbox-grid';
 //js
 import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
-
+import Button from '../../common/Button/Button';
 //data
 import pricing from '../../../data/pricing.json';
+import settings from '../../../data/settings';
+//utils
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+
+const sendOrder = (options, tripCost, tripName, tripId, tripCode) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+  const {contact, name} = options;
+
+  if ( name === '' || name.length < 5 ) {
+    alert('Please fill name with 5 or more letters');
+    return;
+  }
+  if ( contact === '' || contact.length < 5 ) {
+    alert('Please fill contact with 5 or more letters');
+    return;
+  }
+  const payload = {
+    ...options,
+    totalCost,
+    tripName,
+    tripId,
+    tripCode,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 
-const OrderForm = ({ tripCost, options, setOrderOption }) => (
+const OrderForm = ({ tripCost, options, setOrderOption, tripName, tripId, tripCode }) => (
   <Row>
     {pricing.map(option => (
       <Col md={4} key={option.id}>
@@ -21,6 +64,7 @@ const OrderForm = ({ tripCost, options, setOrderOption }) => (
       </Col>))}
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} options={options}/>
+      <Button onClick={() => sendOrder(options, tripCost, tripName, tripId, tripCode)}>Order now!</Button>
     </Col>
   </Row>
 );
@@ -29,6 +73,9 @@ OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  tripId: PropTypes.string,
+  tripName: PropTypes.string,
+  tripCode: PropTypes.string,
 };
 
 export default OrderForm;
