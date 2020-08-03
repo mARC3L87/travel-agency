19308e301205758401682008fcc05bcdf80a118d
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-08-03 23:45:08 +0200

    Change method toString in format time function

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -5,9 +5,9 @@ export const formatTime = time => {
     let seconds = Math.floor(time%60);
     let minutes = Math.floor(time/60)%60;
     let hours = Math.floor(time/3600);
-    hours = String(hours).padStart(2, '0');
-    minutes = String(minutes).padStart(2, '0');
-    seconds = String(seconds).padStart(2, '0');
+    hours = hours.toString().padStart(2, '0');
+    minutes = minutes.toString().padStart(2, '0');
+    seconds = seconds.toString().padStart(2, '0');
     return `${hours}:${minutes}:${seconds}`;
   }
 };
